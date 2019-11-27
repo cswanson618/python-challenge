c6ad5fa860d59413f4cece42429de2618154bd0d
cswanson618 <54300477+cswanson618@users.noreply.github.com>
2019-11-26 19:31:40 -0600

    Cleaned up code and converted to dictionary

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,16 +1,18 @@
-import os
 import csv
 
 #do you need to add something to point this to a directory?
 
-with open('election_data.csv') as e:
-    vote_count = 0
+with open("election_data.csv") as e:
     for row in e:
         rows = sum(1 for row in e)
 
+        print("Election Results")
+        print("------")
+        print("Total Votes: " + str(rows+1))
 
-
-    print("Election Results")
-    print("------")
-    print("Total Votes: " + str(rows+1))
-    print
+with open("election_data.csv") as f:
+    list_dict = csv.DictReader(f)
+    for row in list_dict:
+        
+        print("------")
+        print (Khan)
