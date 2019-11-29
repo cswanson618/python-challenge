9b5fe8f5e3acba15a1ac95134267fa9ffa70adfd
cswanson618 <54300477+cswanson618@users.noreply.github.com>
2019-11-29 10:53:33 -0600

    Realized I read instructions incorrectly. Scripted so Python would count unique candidates and tabulate. Need to rewrite tabulation script but made a few tweaks so script would count unique candidates based on what I already had.

--- pypoll/main.py
+++ pypoll/main.py
@@ -14,48 +14,51 @@ with open("election_data.csv") as e:
 
 with open("election_data.csv") as f:
     cdte = csv.DictReader(f)
+    first_list = []
     can_list = []
     for row in cdte:
         for k, v in row.items():
-                can_list.append(v)
-                k= (can_list.count("Khan"))
-                cor= (can_list.count("Correy"))
-                ot= (can_list.count("O'Tooley"))
-                li= (can_list.count("Li"))
+                first_list.append(v)
 
-print("Election Results")
-print("------")
-print("Total Votes: " + str(rows))
-print("------")
-print("Votes by candidate:")
-print("")
-print ("Khan recieved " + str(k) + " votes, which is " + str(round((k/(rows+1)*100),2))+ "% of the total vote")
-print ("Correy recieved " + str(cor) + " votes, which is " + str(round((cor/(rows+1)*100),2))+ "% of the total vote")
-print ("O'Tooley recieved " + str(ot) + " votes, which is " + str(round((ot/(rows+1)*100),2))+ "% of the total vote")
-print ("Li recieved " + str(li) + " votes, which is " + str(round((li/(rows+1)*100),2))+ "% of the total vote")
-print("------")
-print("Winner is:")
-if (k == cor):
-    print ("Tie between Khan and Correy")
-elif (k == ot):
-    print ("Tie between Khan and O'Tooley")
-elif (k == li):
-    print ("Tie between Khan and Li")
-elif (cor == ot):
-    print ("Tie between Correy and O'Tooley")
-elif (cor == li):
-    print ("Tie between Correy and Li")
-elif (ot == li):
-    print ("Tie between O'Tooley and Li")
-elif (k > cor) and (k > ot) and (k > li):
-    print("Khan")
-elif (cor > k) and (cor > ot) and (cor > li):
-    print("Correy")
-elif (ot > k) and (ot > cor) and (ot > li):
-    print("O'Tooley")
-elif (li > k) and (li > cor) and (li > ot):
-    print("Li")
-else:
-    print("ERROR. Please notify Christopher Swanson.")
-print ("------")
-print ("End of tabulation")
+can_list = first_list [2::3]
+unique_candidates = set(can_list)
+print (unique_candidates)
+
+            
+
+# print("Election Results")
+# print("------")
+# print("Total Votes: " + str(rows))
+# print("------")
+# print("Votes by candidate:")
+# print("")
+# print ("Khan recieved " + str(k) + " votes, which is " + str(round((k/(rows+1)*100),2))+ "% of the total vote")
+# print ("Correy recieved " + str(cor) + " votes, which is " + str(round((cor/(rows+1)*100),2))+ "% of the total vote")
+# print ("O'Tooley recieved " + str(ot) + " votes, which is " + str(round((ot/(rows+1)*100),2))+ "% of the total vote")
+# print ("Li recieved " + str(li) + " votes, which is " + str(round((li/(rows+1)*100),2))+ "% of the total vote")
+# print("------")
+# print("Winner is:")
+# if (k == cor):
+#     print ("Tie between Khan and Correy")
+# elif (k == ot):
+#     print ("Tie between Khan and O'Tooley")
+# elif (k == li):
+#     print ("Tie between Khan and Li")
+# elif (cor == ot):
+#     print ("Tie between Correy and O'Tooley")
+# elif (cor == li):
+#     print ("Tie between Correy and Li")
+# elif (ot == li):
+#     print ("Tie between O'Tooley and Li")
+# elif (k > cor) and (k > ot) and (k > li):
+#     print("Khan")
+# elif (cor > k) and (cor > ot) and (cor > li):
+#     print("Correy")
+# elif (ot > k) and (ot > cor) and (ot > li):
+#     print("O'Tooley")
+# elif (li > k) and (li > cor) and (li > ot):
+#     print("Li")
+# else:
+#     print("ERROR. Please notify Christopher Swanson.")
+# print ("------")
+# print ("End of tabulation")
